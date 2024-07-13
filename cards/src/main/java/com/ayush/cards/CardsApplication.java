a7756373fd2dc2ceede5a19e5159cc2ac8e9d83b
Ayush Keshri <keshri.ayush019@gmail.com>
2024-07-13 18:33:25 +0530

    Added Cards microservice

--- cards/src/main/java/com/ayush/cards/CardsApplication.java
+++ cards/src/main/java/com/ayush/cards/CardsApplication.java
@@ -1,13 +1,31 @@
 package com.ayush.cards;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Cards microservice REST API Documentation",
+		description = "Cards microservice CRUD REST API Documentation",
+		version = "v1",
+		contact = @Contact(
+			name = "Ayush Keshri",
+			email = "ayushkeshri275@gmail.com"
+		),
+		license = @License(
+			name = "Apache 2.0"
+		)
+	)
+)
 public class CardsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CardsApplication.class, args);
 	}
-
 }
---
+++ cards/src/main/java/com/ayush/cards/service/ICardsService.java
@@ -0,0 +1,10 @@
+package com.ayush.cards.service;
+
+import com.ayush.cards.dto.CardsDto;
+
+public interface ICardsService {
+    void createCard(String mobileNumber);
+    CardsDto fetchCard(String mobileNumber);
+    boolean updateCard(CardsDto cardsDto);
+    boolean deleteCard(String mobileNumber);
+}
